ab15cd11d7323971641d67485320432ecb88f47f
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2023-05-22 18:51:45 -0300

    :sparkles: feat: Add commented Notification code

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 
+// import { NotificationContextProvider } from '@/features/notification'
 import { Provider } from 'react-redux'
 import store from '@/store'
 import App from '@/pages/App'
@@ -11,10 +12,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
+    {/* <NotificationContextProvider> */}
     <Provider store={store}>
       <Router>
         <App />
       </Router>
     </Provider>
+    {/* </NotificationContextProvider> */}
   </React.StrictMode>
 )
